326bc0f73de6cf943ffa06e75146faeab77d5728
AlisonChs <alison.carvalho@etec.sp.gov.br>
2022-08-05 19:37:25 -0300

    first commit

---
+++ chamada/src/main/java/com/chamada/chs/chamada/ChamadaApplication.java
@@ -0,0 +1,13 @@
+package com.chamada.chs.chamada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadaApplication.class, args);
+	}
+
+}
