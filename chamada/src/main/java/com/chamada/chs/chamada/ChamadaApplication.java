c364db57ab7dfa501f979b6614482dcb72fae3e6
AlisonChs <alison.carvalho@etec.sp.gov.br>
2022-08-10 05:22:43 -0300

    Create CRUD tests

--- chamada/src/main/java/com/chamada/chs/chamada/ChamadaApplication.java
+++ chamada/src/main/java/com/chamada/chs/chamada/ChamadaApplication.java
@@ -2,6 +2,7 @@ package com.chamada.chs.chamada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ChamadaApplication {
